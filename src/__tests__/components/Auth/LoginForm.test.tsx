5435b4a3287860b002f99c534a613cad32206730
Nikita Kapshtyk <viks2332@gmail.com>
2021-09-22 17:18:48 +0300

    simple auth forms tests

--- src/__tests__/components/Auth/LoginForm.test.tsx
+++ src/__tests__/components/Auth/LoginForm.test.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { render, waitFor } from '@testing-library/react';
-import LoginForm from 'components/Auth/LoginForm/LoginForm';
+import React from "react";
+import { render, waitFor, fireEvent } from "@testing-library/react";
+import LoginForm from "components/Auth/LoginForm/LoginForm";
 
-describe('LoginForm', () => {
-  test('Simple usage', async () => {
+describe("LoginForm", () => {
+  test("Simple usage", async () => {
     const component = render(<LoginForm />);
 
-    await waitFor(() => component.getByTestId('auth-login-form'));
+    await waitFor(() => component.getByTestId("auth-login-form"));
+
+    await waitFor(() => {
+      expect(component.getByTestId("auth-login-form-email")).toHaveValue("");
+      expect(component.getByTestId("auth-login-form-password")).toHaveValue("");
+    });
+    fireEvent.change(component.getByTestId("auth-login-form-email"), {
+      target: { value: "Some custom email" },
+    });
+    await waitFor(() => {
+      expect(component.getByTestId("auth-login-form-email")).toHaveValue("Some custom email");
+    });
+
+    fireEvent.click(component.getByTestId("auth-login-form-submit"));
+    //TODO - add tests for request
   });
 });
--- src/components/Auth/RegisterForm/RegisterForm.tsx
+++ src/components/Auth/RegisterForm/RegisterForm.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import TextInput from '../../Generic/TextInput/TextInput';
-import Button, { ButtonStyle } from '../../Generic/Button/Button';
+import React from "react";
+import { useForm } from "react-hook-form";
+import TextInput from "../../Generic/TextInput/TextInput";
+import Button, { ButtonStyle } from "../../Generic/Button/Button";
 
 interface RegisterFormInputs {
   email: string;
@@ -21,38 +21,41 @@ export default function RegisterForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} data-ui-purpose="auth-register-form">
       <TextInput
+        testId="auth-register-form-email"
         placeholder="Enter your email"
         label="Email"
-        {...register('email', {
-          required: 'This field is required',
+        {...register("email", {
+          required: "This field is required",
           pattern: {
             value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-            message: 'Please enter a valid email',
+            message: "Please enter a valid email",
           },
         })}
         error={errors.email?.message}
       />
       <TextInput
+        testId="auth-register-form-username"
         placeholder="Enter your username"
         label="Username"
-        {...register('username', {
-          required: 'This field is required',
+        {...register("username", {
+          required: "This field is required",
         })}
         error={errors.username?.message}
       />
       <TextInput
+        testId="auth-register-form-password"
         type="password"
         placeholder="Enter your password"
         label="Password"
-        {...register('password', {
-          required: 'This field is required',
+        {...register("password", {
+          required: "This field is required",
         })}
         error={errors.password?.message}
       />
       <div className="tw-flex tw-justify-center tw-pt-3">
-        <Button type="submit" style={ButtonStyle.green}>
+        <Button type="submit" style={ButtonStyle.green} testId="auth-register-form-submit">
           Register
         </Button>
       </div>
